3aa5776f63325f0839e402a17fdcd2f0cd7492f3
Roman <hardyroman@gmail.com>
2022-04-26 13:12:54 +0200

    plus de tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,48 @@
 # Note that this assumes to be called from the Makefile, you may want to adapt it.
-echo "Test with simlink"
-./tests/test.sh tests/small.txt 5 5 5 5 5
-# Run the same test, but this time with valgrind
-echo "A very simple test, with Valgrind"
-VALGRIND=1 ./tests/test.sh tests/small.txt 10 10 10 10 10
+# To add a test, create a directory with the name of the test (to store the log and stats files)
+# then run test.sh 
+total=0
+failed=0
+
+echo "Basic test with simlink inactive and text file"
+./tests/test.sh tests/test.txt 0 0 0 0 0 basic
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with simlink and text file"
+./tests/test.sh tests/small.txt 10 5 5 5 5 smallSimlink
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with random text file"
+./tests/test.sh tests/random.txt 10 5 5 5 5 random
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with small jpg file on bad network"
+./tests/test.sh tests/smallImage.jpg 50 5 5 2 10 smallImage
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with jpg file on perfect network"
+./tests/test.sh tests/image.jpg 0 0 0 0 0 image
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with simlink and jpg file"
+./tests/test.sh tests/image.jpg 10 2 2 2 3 imageSimlink
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with pdf file on perfect network"
+./tests/test.sh tests/report_template_a3.pdf 0 0 0 0 0 pdf
+failed=$(($failed + $?))
+((total++))
+
+echo "Test with simlink and pdf file"
+./tests/test.sh tests/report_template_a3.pdf 10 3 3 3 10 pdfSimlink
+failed=$(($failed + $?))
+((total++))
+
+echo "==== ${failed} transfert(s) raté(s) sur ${total} tests ===="
+echo ""
--- tests/test.sh
+++ tests/test.sh
@@ -4,6 +4,23 @@
 rm -f received_file input_file
 
 input_file=$1
+test_name=$7
+
+echo "=== Test with file ${input_file} ==="
+
+filesize=$(ls -lh $input_file | awk '{print  $5}')
+echo "Size of the file : ${filesize}"
+
+receiver_log="./tests/${test_name}/receiver.log"
+receiver_csv="./tests/${test_name}/receiver.csv"
+
+sender_log="./tests/${test_name}/sender.log"
+sender_csv="./tests/${test_name}/sender.csv"
+
+linksim_log="./tests/${test_name}/linksim.log"
+
+rm -f sender_log receiver_log sender_csv receiver_csv linksim_log
+
 
 # get size in bytes
 mysize=$(find "$input_file" -printf "%s")
@@ -18,12 +35,14 @@ loss=$6
 
 # On lance le simulateur de lien avec 10% de pertes et un délais de 50ms
 echo "link_sim démarré"
-./link_sim -p 2000 -P 2456 -l $loss -d $delay -j $jitter -e $error -c $cut -R &
+
+./link_sim -p 2000 -P 2456 -l $loss -d $delay -j $jitter -e $error -c $cut -R 2> $linksim_log &
+
 link_pid=$!
 
 # On lance le receiver et capture sa sortie standard
-echo "receiver démarré"
-./receiver :: 2456 1>received_file &
+echo "Receiver démarré"
+./receiver -s $receiver_csv :: 2456 1>received_file 2>$receiver_log &
 receiver_pid=$!
 
 cleanup()
@@ -35,16 +54,20 @@ cleanup()
 trap cleanup SIGINT  # Kill les process en arrière plan en cas de ^-C
 
 # On démarre le transfert
-echo "sender démarré"
 
+echo "Sender démarré"
 
-if ! time_sender=$(/usr/bin/time --format="%e" ./sender -f $input_file ::1 2000 2>&1|tail -n 1) ; then
+/usr/bin/time --format="%e" ./sender -f $input_file -s $sender_csv ::1 2000 2>$sender_log
+
+if ! time_sender=$(tail $sender_log -n 1) ; then
   echo "Crash du sender!"
-  cat sender.log
   err=1  # On enregistre l'erreur
 fi
 
+echo "Execution time of the sender : ${time_sender}"
+echo "En attente que le receiver finisse"
 sleep 5 # On attend 5 seconde que le receiver finisse
+
 
 if kill -0 $receiver_pid &> /dev/null ; then
   echo "Le receiver ne s'est pas arreté à la fin du transfert!"
@@ -65,13 +88,13 @@ sleep 1
 
 # On vérifie que le transfert s'est bien déroulé
 if [[ "$(sha256sum $input_file | awk '{print $1}')" != "$(sha256sum received_file | awk '{print $1}')" ]]; then
-  echo "Le transfert a corrompu le fichier!"
-  echo "Diff binaire des deux fichiers: (attendu vs produit)"
-  diff -C 9 <(od -Ax -t x1z input_file) <(od -Ax -t x1z received_file)
+  echo " --> Le transfert a corrompu le fichier!"
+  #echo "Diff binaire des deux fichiers: (attendu vs produit)"
+  #diff -C 9 <(od -Ax -t x1z input_file) <(od -Ax -t x1z received_file)
   corrompu=1
 else
   corrompu=0
-  echo "Le transfert est réussi!"
+  echo " --> Le transfert est réussi!"
 fi
 
 if [ -s time_to_sender.csv ]
@@ -81,3 +104,8 @@ else
   echo "extension ,size ,time_sender ,loss ,delay ,jitter ,error ,cut ,pkt_corrupted," >> time_to_sender.csv
   echo "$extension ,$mysize, $time_sender ,$loss ,$delay ,$jitter ,$error ,$cut, $corrompu," >> time_to_sender.csv
 fi
+
+
+echo ""
+exit $corrompu
+
